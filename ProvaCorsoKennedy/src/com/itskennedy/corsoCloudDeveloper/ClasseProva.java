6e7fb1c3787a7ff41925bf67b54cc63b1461f490
ITSBuraM <ITSBuraM@itscor207246.itsacademy.corvallisholding.it>
2021-03-03 10:00:53 +0100

    secondo commit

--- ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
+++ ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -2,4 +2,11 @@ package com.itskennedy.corsoCloudDeveloper;
 
 public class ClasseProva {
 
+	public static void main(String[] args) {
+		System.out.println("ciao a tutti");
+	}
+	
+	private void test() {
+		System.out.println("");
+	}
 }
